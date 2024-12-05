d497df4dd719dd9e731884f2c01ac2487749f128
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-05 22:06:44 +0330

    feat: added services order, notification, payment, inventory

--- inventory-service/src/app.controller.ts
+++ inventory-service/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @MessagePattern('get-hello')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
