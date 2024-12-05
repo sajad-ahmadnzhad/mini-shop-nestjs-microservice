53a669f59568369ab7e584f108aec4b1352a71d4
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-05 16:16:47 +0330

    feat: add swagger in gateway

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { swaggerConfigInit } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  swaggerConfigInit(app)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
