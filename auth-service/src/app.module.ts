3fc8cb75812c9e6d0542545af2819017b9cf84b1
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-06 18:57:58 +0330

    feat: add env config in auth service

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from './configs/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(envConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth-service/src/configs/env.config.ts
@@ -0,0 +1,31 @@
+import { ConfigModuleOptions } from "@nestjs/config";
+import * as Joi from "joi";
+
+export default (): ConfigModuleOptions => {
+    return {
+        isGlobal: true,
+        envFilePath: process.cwd() + "/.env",
+        validate: (config: Record<string, any>) => {
+            const schema = Joi.object({
+                GMAIL_USER: Joi.string().required(),
+                GMAIL_PASS: Joi.string().required(),
+                ACCESS_TOKEN_SECRET: Joi.string().required(),
+                REFRESH_TOKEN_SECRET: Joi.string().required(),
+                ACCESS_TOKEN_EXPIRE_TIME: Joi.string().required(),
+                REFRESH_TOKEN_EXPIRE_TIME: Joi.string().required(),
+                GOOGLE_CLIENT_ID: Joi.string().required(),
+                GOOGLE_CLIENT_SECRET: Joi.string().required(),
+                GOOGLE_CALLBACK_URL: Joi.string().required(),
+            }).unknown(true)
+
+            const { error, value } = schema.validate(config)
+
+            if (error) {
+                console.error('Env validation error:', error)
+                process.exit(1)
+            }
+
+            return value
+        }
+    }
+}
