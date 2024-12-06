045c36103f9cb6d496cd52918a252ac5218f7e8b
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-06 15:52:08 +0330

    feat: add swagger in gateway and add yarn lock in services

---
+++ gateway/src/configs/swagger.config.ts
@@ -0,0 +1,27 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SecuritySchemeObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
+
+export const swaggerConfigInit = (app: INestApplication) => {
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle("Mini online shop")
+        .addBearerAuth(swaggerAuthConfig(), "Authorization")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig, {
+        ignoreGlobalPrefix: true,
+    });
+
+    SwaggerModule.setup("swagger", app, document, {
+        jsonDocumentUrl: "swagger/json",
+    });
+};
+
+function swaggerAuthConfig(): SecuritySchemeObject {
+    return {
+        scheme: "bearer",
+        type: "http",
+        in: "header",
+        bearerFormat: "JWT",
+    };
+}
--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { swaggerConfigInit } from './configs/swagger.config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  swaggerConfigInit(app)
+
+  const logger = new Logger("NestApplication")
+
+  const { PORT = 4000 } = process.env
+
+  await app.listen(PORT, () => {
+    logger.log(`Gateway running on port ${PORT}`)
+  });
 }
 bootstrap();
