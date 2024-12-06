7616553544e2ecdf6ec8dbfdcc63bdcf2226d8b6
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-06 16:44:01 +0330

    feat: add env config file in gateway

---
+++ gateway/src/configs/env.config.ts
@@ -0,0 +1,22 @@
+import { ConfigModuleOptions } from "@nestjs/config";
+import * as Joi from "joi";
+
+export default (): ConfigModuleOptions => {
+    return {
+        isGlobal: true,
+        validate: (config: Record<string, any>) => {
+            const schema = Joi.object({
+                PORT: Joi.string().required()
+            }).unknown(true)
+
+            const { error, value } = schema.validate(config)
+
+            if (error) {
+                console.error('Env validation error:', error)
+                process.exit(1)
+            }
+
+            return value
+        }
+    }
+}
--- gateway/src/modules/app/app.module.ts
+++ gateway/src/modules/app/app.module.ts
@@ -1,10 +1,64 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from 'src/configs/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(envConfig()),
+    ClientsModule.register({
+      isGlobal: true,
+      clients: [
+        {
+          name: "ORDER_SERVICE",
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: "order-service",
+            queueOptions: {
+              durable: false,
+            }
+          }
+        },
+        {
+          name: "PAYMENT_SERVICE",
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: "payment-service",
+            queueOptions: {
+              durable: false,
+            }
+          }
+        },
+        {
+          name: "NOTIFICATION_SERVICE",
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: "notification-service",
+            queueOptions: {
+              durable: false,
+            }
+          }
+        },
+        {
+          name: "INVENTORY_SERVICE",
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: "inventory-service",
+            queueOptions: {
+              durable: false,
+            }
+          }
+        },
+      ]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
