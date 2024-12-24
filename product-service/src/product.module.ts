4bb3b05b7f4ae5538a3045c238e7af0ed6b7c5ae
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-24 22:01:43 +0330

    feat: add typeorm config

--- product-service/src/product.module.ts
+++ product-service/src/product.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeormConfig } from './configs/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeormConfig())
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
