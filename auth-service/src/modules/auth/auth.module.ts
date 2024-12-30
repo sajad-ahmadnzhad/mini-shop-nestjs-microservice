32eeab01f39b897a8020c11db23939aaa804aaf7
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-12-30 21:00:57 +0330

    feat: add notification service

--- auth-service/src/modules/auth/auth.module.ts
+++ auth-service/src/modules/auth/auth.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
-import envConfig from '../../configs/env.config';
-import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeormConfig } from '../../configs/typeorm.config';
-import { CacheModule } from '@nestjs/cache-manager';
-import cacheConfig from '../../configs/cache.config';
-import { RoleModule } from '../role/role.module';
-import { UsersModule } from '../users/users.module';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { ConfigModule } from "@nestjs/config";
+import envConfig from "../../configs/env.config";
+import { JwtModule } from "@nestjs/jwt";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeormConfig } from "../../configs/typeorm.config";
+import { CacheModule } from "@nestjs/cache-manager";
+import cacheConfig from "../../configs/cache.config";
+import { RoleModule } from "../role/role.module";
+import { UsersModule } from "../users/users.module";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
   imports: [
@@ -20,9 +21,22 @@ import { UsersModule } from '../users/users.module';
       global: true,
     }),
     RoleModule,
-    UsersModule
+    UsersModule,
+    ClientsModule.register({
+      isGlobal: true,
+      clients: [
+        {
+          name: "NOTIFICATION_SERVICE",
+          transport: Transport.RMQ,
+          options: {
+            urls: [process.env.RABBITMQ_URL],
+            queue: process.env.RABBITMQ_NOTIFICATION_SERVICE_QUEUE,
+          },
+        },
+      ],
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
