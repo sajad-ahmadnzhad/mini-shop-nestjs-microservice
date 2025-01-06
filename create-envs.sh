e010027601738a3864ede22b6ed3c7d9b5bf0c21
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-06 13:46:59 +0330

    feat: add create envs shell script file

---
+++ create-envs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Env name and files
+ENV_NAME=".env"
+EXPORT_FILE=".env.example"
+
+# Services list
+SERVICES=("auth-service" "gateway" "notification-service" "product-service")
+
+for SERVICE in "${SERVICES[@]}"; do
+    echo "Processing $SERVICE..."
+    #Varilables for save .env and .env.exmaple paths 
+    SERVICE_ENV_PATH="$SERVICE/$ENV_NAME"
+    SERVICE_EXPORT_PATH="$SERVICE/$EXPORT_FILE"
+    
+    # Check for exists service
+    if [ -d "$SERVICE" ]; then
+        # Check for exists .env file in service 
+        if [ ! -f "$SERVICE_ENV_PATH" ]; then
+            # Check for exists .env.example file in service
+            if [ ! -f "$SERVICE_EXPORT_PATH" ]; then
+                echo "$EXPORT_FILE not found in $SERVICE"
+                exit 1
+            else 
+                #Copy .env.exmaple file data in .env file
+                cp "$SERVICE_EXPORT_PATH" "$SERVICE_ENV_PATH"
+                echo "$SERVICE_ENV_PATH file created from $SERVICE_EXPORT_PATH"
+            fi
+        else
+            echo "$SERVICE already added $ENV_NAME"
+    fi
+    fi
+done
